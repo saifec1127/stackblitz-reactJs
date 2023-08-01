6deadb237b25311b0d13602cb218a99599e03122
saifec1127 <49020316+saifec1127@users.noreply.github.com>
2023-08-01 13:50:51 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,83 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  const [win, setWin] = useState('');
+  const [wins, setWins] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const HandleChange = (event) => {
+    setWin(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (win === '' || selectedOption === '') {
+      setErrorMessage('Both fields are required to submit the form.');
+    } else {
+      setWins([...wins, { id: win.length, win: win, option: selectedOption }]);
+      setWin('');
+      setSelectedOption('');
+      setErrorMessage('');
+    }
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello Voters!</h1>
+      <form onSubmit={handleSubmit}>
+        <lable>
+          Name
+          <input
+            type="text"
+            placeholder="Write here"
+            value={win}
+            onChange={HandleChange}
+          />
+        </lable>
+        <br />
+        <br />
+        <label>
+          <input
+            type="radio"
+            name="india"
+            value="INDIA"
+            checked={selectedOption === 'INDIA'}
+            onChange={handleOptionChange}
+          />
+          INDIA
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="nda"
+            value="NDA"
+            checked={selectedOption === 'NDA'}
+            onChange={handleOptionChange}
+          />
+          NDA
+        </label>
+        <br />
+        <br />
+        <button>Click</button>
+      </form>
+      {errorMessage && <p className="alert_p">{errorMessage}</p>}
+      <div>
+        {wins.map((item) => {
+          return (
+            <p key={item.id}>
+              {item.win + ' ' + (item.option ? item.option : '')}
+            </p>
+          );
+        })}
+      </div>
     </div>
   );
 }
